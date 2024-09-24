f5c9e5f824bf6a52b365c0f08657dabeb55d187a
a-rodriguez-c <ja.rodriguezc12@uniandes.edu.co>
2024-09-23 22:04:31 -0500

    adding task view

--- src/app/task/task.module.ts
+++ src/app/task/task.module.ts
@@ -7,6 +7,7 @@ import { TaskPage } from './task.page';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatIconModule} from '@angular/material/icon';
 import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
 
 import { TaskPageRoutingModule } from './task-routing.module';
 
@@ -19,7 +20,8 @@ import { TaskPageRoutingModule } from './task-routing.module';
     TaskPageRoutingModule,
     MatFormFieldModule,
     MatIconModule,
-    MatInputModule
+    MatInputModule,
+    MatButtonModule
   ],
   declarations: [TaskPage]
 })
